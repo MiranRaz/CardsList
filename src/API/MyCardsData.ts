ad018ff345b9cc20adeca6ae57fd6ece4ead00f9
Miran Raznatovic <raznatovicmiran@gmail.com>
2023-05-29 11:02:40 +0200

    WIP for image path not being able to open

--- src/API/MyCardsData.ts
+++ src/API/MyCardsData.ts
@@ -13,7 +13,8 @@ export const cardData: CardData[] = [
     description:
       'Contrary to popular belief, Lorem Ipsum is not simply random text. It has roots in a piece of classical Latin literature from 45 BC, making it over 2000 years old. Richard McClintock, a Latin professor at Hampden-Sydney College in Virginia, looked up one of the more obscure Latin words, consectetur, from a Lorem Ipsum passage, and going through the cites of the word in classical literature, discovered the undoubtable source. Lorem Ipsum comes from sections 1.10.32 and 1.10.33 of "de Finibus Bonorum et Malorum" (The Extremes of Good and Evil) by Cicero, written in 45 BC. This book is a treatise on the theory of ethics, very popular during the Renaissance. The first line of Lorem Ipsum, "Lorem ipsum dolor sit amet..", comes from a line in section 1.10.32.\n' +
       'The standard chunk of Lorem Ipsum used since the 1500s is reproduced below for those interested. Sections 1.10.32 and 1.10.33 from "de Finibus Bonorum et Malorum" by Cicero are also reproduced in their exact original form, accompanied by English versions from the 1914 translation by H. Rackham.',
-    image: "src/assets/food.jpg",
+    image:
+      "https://raw.githubusercontent.com/MiranRaz/CardsList/main/src/assets/food.jpg?token=GHSAT0AAAAAACDHKKECTTGCIG6OK2DYVYDEZDUNGDQ",
     hash: "LMK]Z4?slRb_Y%.7O?GG?I-C=y=|",
   },
   {
@@ -21,7 +22,8 @@ export const cardData: CardData[] = [
     title: "Card 2",
     description:
       "There are many variations of passages of Lorem Ipsum available, but the majority have suffered alteration in some form, by injected humour, or randomised words which don't look even slightly believable. If you are going to use a passage of Lorem Ipsum, you need to be sure there isn't anything embarrassing hidden in the middle of text. All the Lorem Ipsum generators on the Internet tend to repeat predefined chunks as necessary, making this the first true generator on the Internet. It uses a dictionary of over 200 Latin words, combined with a handful of model sentence structures, to generate Lorem Ipsum which looks reasonable. The generated Lorem Ipsum is therefore always free from repetition, injected humour, or non-characteristic words etc.",
-    image: "src/assets/food.jpg",
+    image:
+      "https://raw.githubusercontent.com/MiranRaz/CardsList/main/src/assets/food.jpg?token=GHSAT0AAAAAACDHKKECTTGCIG6OK2DYVYDEZDUNGDQ",
     hash: "LMK]Z4?slRb_Y%.7O?GG?I-C=y=|",
   },
   {
@@ -29,7 +31,8 @@ export const cardData: CardData[] = [
     title: "Card 3",
     description:
       "All the Lorem Ipsum generators on the Internet tend to repeat predefined chunks as necessary, making this the first true generator on the Internet. It uses a dictionary of over 200 Latin words, combined with a handful of model sentence structures, to generate Lorem Ipsum which looks reasonable. The generated Lorem Ipsum is therefore always free from repetition, injected humour, or non-characteristic words etc.",
-    image: "src/assets/food.jpg",
+    image:
+      "https://raw.githubusercontent.com/MiranRaz/CardsList/main/src/assets/food.jpg?token=GHSAT0AAAAAACDHKKECTTGCIG6OK2DYVYDEZDUNGDQ",
     hash: "LMK]Z4?slRb_Y%.7O?GG?I-C=y=|",
   },
   {
@@ -37,7 +40,8 @@ export const cardData: CardData[] = [
     title: "Card 4",
     description:
       "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.",
-    image: "src/assets/food.jpg",
+    image:
+      "https://raw.githubusercontent.com/MiranRaz/CardsList/main/src/assets/food.jpg?token=GHSAT0AAAAAACDHKKECTTGCIG6OK2DYVYDEZDUNGDQ",
     hash: "LMK]Z4?slRb_Y%.7O?GG?I-C=y=|",
   },
   {
@@ -45,7 +49,8 @@ export const cardData: CardData[] = [
     title: "Card 5",
     description:
       "Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam rem aperiam, eaque ipsa quae ab illo inventore veritatis et quasi architecto beatae vitae dicta sunt explicabo. Nemo enim ipsam voluptatem quia voluptas sit aspernatur aut odit aut fugit, sed quia consequuntur magni dolores eos qui ratione voluptatem sequi nesciunt. Neque porro quisquam est, qui dolorem ipsum quia dolor sit amet, consectetur, adipisci velit, sed quia non numquam eius modi tempora incidunt ut labore et dolore magnam aliquam quaerat voluptatem. Ut enim ad minima veniam, quis nostrum exercitationem ullam corporis suscipit laboriosam, nisi ut aliquid ex ea commodi consequatur? Quis autem vel eum iure reprehenderit qui in ea voluptate velit esse quam nihil molestiae consequatur, vel illum qui dolorem eum fugiat quo voluptas nulla pariatur?",
-    image: "src/assets/food.jpg",
+    image:
+      "https://raw.githubusercontent.com/MiranRaz/CardsList/main/src/assets/food.jpg?token=GHSAT0AAAAAACDHKKECTTGCIG6OK2DYVYDEZDUNGDQ",
     hash: "LMK]Z4?slRb_Y%.7O?GG?I-C=y=|",
   },
   {
@@ -53,7 +58,8 @@ export const cardData: CardData[] = [
     title: "Card 6",
     description:
       "But I must explain to you how all this mistaken idea of denouncing pleasure and praising pain was born and I will give you a complete account of the system, and expound the actual teachings of the great explorer of the truth, the master-builder of human happiness. No one rejects, dislikes, or avoids pleasure itself, because it is pleasure, but because those who do not know how to pursue pleasure rationally encounter consequences that are extremely painful. Nor again is there anyone who loves or pursues or desires to obtain pain of itself, because it is pain,",
-    image: "src/assets/food.jpg",
+    image:
+      "https://raw.githubusercontent.com/MiranRaz/CardsList/main/src/assets/food.jpg?token=GHSAT0AAAAAACDHKKECTTGCIG6OK2DYVYDEZDUNGDQ",
     hash: "LMK]Z4?slRb_Y%.7O?GG?I-C=y=|",
   },
   {
@@ -61,77 +67,88 @@ export const cardData: CardData[] = [
     title: "Card 7",
     description:
       "but because occasionally circumstances occur in which toil and pain can procure him some great pleasure. To take a trivial example, which of us ever undertakes laborious physical exercise, except to obtain some advantage from it? But who has any right to find fault with a man who chooses to enjoy a pleasure that has no annoying consequences, or one who avoids a pain that produces no resultant pleasure?",
-    image: "src/assets/food.jpg",
+    image:
+      "https://raw.githubusercontent.com/MiranRaz/CardsList/main/src/assets/food.jpg?token=GHSAT0AAAAAACDHKKECTTGCIG6OK2DYVYDEZDUNGDQ",
     hash: "LMK]Z4?slRb_Y%.7O?GG?I-C=y=|",
   },
   {
     id: 8,
     title: "Card 8",
     description: "This is the description for Card 8.",
-    image: "src/assets/food.jpg",
+    image:
+      "https://raw.githubusercontent.com/MiranRaz/CardsList/main/src/assets/food.jpg?token=GHSAT0AAAAAACDHKKECTTGCIG6OK2DYVYDEZDUNGDQ",
     hash: "LMK]Z4?slRb_Y%.7O?GG?I-C=y=|",
   },
   {
     id: 9,
     title: "Card 9",
     description: "This is the description for Card 9.",
-    image: "src/assets/food.jpg",
+    image:
+      "https://raw.githubusercontent.com/MiranRaz/CardsList/main/src/assets/food.jpg?token=GHSAT0AAAAAACDHKKECTTGCIG6OK2DYVYDEZDUNGDQ",
     hash: "LMK]Z4?slRb_Y%.7O?GG?I-C=y=|",
   },
   {
     id: 10,
     title: "Card 10",
     description: "This is the description for Card 10.",
-    image: "src/assets/food.jpg",
+    image:
+      "https://raw.githubusercontent.com/MiranRaz/CardsList/main/src/assets/food.jpg?token=GHSAT0AAAAAACDHKKECTTGCIG6OK2DYVYDEZDUNGDQ",
     hash: "LMK]Z4?slRb_Y%.7O?GG?I-C=y=|",
   },
   {
     id: 11,
     title: "Card 11",
     description: "This is the description for Card 11.",
-    image: "src/assets/food.jpg",
+    image:
+      "https://raw.githubusercontent.com/MiranRaz/CardsList/main/src/assets/food.jpg?token=GHSAT0AAAAAACDHKKECTTGCIG6OK2DYVYDEZDUNGDQ",
     hash: "LMK]Z4?slRb_Y%.7O?GG?I-C=y=|",
   },
   {
     id: 12,
     title: "Card 12",
     description: "This is the description for Card 12.",
-    image: "src/assets/food.jpg",
+    image:
+      "https://raw.githubusercontent.com/MiranRaz/CardsList/main/src/assets/food.jpg?token=GHSAT0AAAAAACDHKKECTTGCIG6OK2DYVYDEZDUNGDQ",
     hash: "LMK]Z4?slRb_Y%.7O?GG?I-C=y=|",
   },
   {
     id: 13,
     title: "Card 13",
     description: "This is the description for Card 13.",
-    image: "src/assets/food.jpg",
+    image:
+      "https://raw.githubusercontent.com/MiranRaz/CardsList/main/src/assets/food.jpg?token=GHSAT0AAAAAACDHKKECTTGCIG6OK2DYVYDEZDUNGDQ",
     hash: "LMK]Z4?slRb_Y%.7O?GG?I-C=y=|",
   },
   {
     id: 14,
     title: "Card 14",
     description: "This is the description for Card 14.",
-    image: "src/assets/food.jpg",
+    image:
+      "https://raw.githubusercontent.com/MiranRaz/CardsList/main/src/assets/food.jpg?token=GHSAT0AAAAAACDHKKECTTGCIG6OK2DYVYDEZDUNGDQ",
     hash: "LMK]Z4?slRb_Y%.7O?GG?I-C=y=|",
   },
   {
     id: 15,
     title: "Card 15",
     description: "This is the description for Card 15.",
-    image: "src/assets/food.jpg",
+    image:
+      "https://raw.githubusercontent.com/MiranRaz/CardsList/main/src/assets/food.jpg?token=GHSAT0AAAAAACDHKKECTTGCIG6OK2DYVYDEZDUNGDQ",
     hash: "LMK]Z4?slRb_Y%.7O?GG?I-C=y=|",
   },
   {
     id: 16,
     title: "Card 16",
     description: "This is the description for Card 16.",
-    image: "src/assets/food.jpg",
+    image:
+      "https://raw.githubusercontent.com/MiranRaz/CardsList/main/src/assets/food.jpg?token=GHSAT0AAAAAACDHKKECTTGCIG6OK2DYVYDEZDUNGDQ",
     hash: "LMK]Z4?slRb_Y%.7O?GG?I-C=y=|",
   },
   {
     id: 17,
     title: "Card 17",
     description: "This is the description for Card 17.",
-    image: "src/assets/food.jpg",
+    image:
+      "https://raw.githubusercontent.com/MiranRaz/CardsList/main/src/assets/food.jpg?token=GHSAT0AAAAAACDHKKECTTGCIG6OK2DYVYDEZDUNGDQ",
     hash: "LMK]Z4?slRb_Y%.7O?GG?I-C=y=|",
   },
 ];
